ab03b0090afbff7e6c67c32d7d5a37c014f405db
Andreas Nilsson <codr7@protonmail.com>
2025-02-10 02:00:35 +0100

    Extract table tests.

---
+++ tests/codr7/tyred/TableTest.java
@@ -0,0 +1,28 @@
+package codr7.tyred;
+
+import codr7.tyred.columns.StringColumn;
+import org.junit.jupiter.api.Test;
+
+class TableTest {
+    static class S extends Schema {
+        final Table users = add(new Table("users"));
+        final StringColumn userName = new StringColumn(users, "name", 100, Option.PrimaryKey);
+
+        final Table admins = add(new Table("admins"));
+        final ForeignKey adminUserKey = new ForeignKey(admins, "user", users, Option.PrimaryKey);
+    }
+
+    @Test
+    public void testUpdate() {
+        final var cx = new Context("tyred", "tyred", "tyred");
+        final var db = new S();
+        db.migrate(cx);
+
+        final var u = new Record();
+        u.set(db.userName, "foo");
+        db.users.insert(u, cx);
+        u.set(db.userName, "bar");
+        db.users.update(u, cx);
+        cx.rollback();
+    }
+}
